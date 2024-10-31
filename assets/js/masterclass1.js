4e09b8f4b9730ac8a9360907df769f2a5fbc8ff9
bo-nicolaisen <bo@squirrelworks.dk>
2024-10-31 21:59:44 +0100

    nu med dom og bind

--- assets/js/masterclass1.js
+++ assets/js/masterclass1.js
@@ -51,7 +51,7 @@ console.log(person.age);
 person.age = 30;
 console.log(person.age);
 
-*/
+
 
 class Person{
 
@@ -72,3 +72,48 @@ let person = new Person('John', 25);
 console.log(person);
 
 person.sayHi();
+*/
+
+
+
+// bind method to class
+
+const personContainer = document.getElementById('app');
+
+class Person{
+
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+
+        // binding the method to this class for the listner
+        this.sayHi = this.sayHi.bind(this);
+
+        this.element= document.createElement('div');
+        this.element.innerText = 'Person';
+        this.element.classList.add('person');
+
+        this.element.addEventListener('click', this.sayHi);
+
+        personContainer.appendChild(this.element);
+
+    }
+
+
+    sayHi(){
+        //console.log(this);
+        this.element.classList.toggle('personActive');
+       this.element.innerText=`Hello, my name is ${this.name} and i am ${this.age} years old`;
+       this.element.removeEventListener('click', this.sayHi);
+    }
+
+}
+
+new Person('ib', 25);
+new Person('keld', 12);
+new Person('søren', 45);
+new Person('bolette', 345);
+
+
+
+
